84e1fb45994b3e6591add5ee72fcf6e49485f785
Muddassir <muddassir.books@gmail.com>
2018-09-03 16:22:29 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Muddassir_restapi_starter
---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\User;
+
+class UserController extends  FOSRestController
+{
+    /**
+     * @Rest\Get("/user")
+     */
+    public function getAction()
+    {
+        $restresult = $this->getDoctrine()->getRepository(User::class)->findAll();
+//        if ($restresult === null) {
+//            return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+//        }
+        return $restresult;
+    }
+}
